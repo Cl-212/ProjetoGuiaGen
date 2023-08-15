2fc175c7f33f8749ebdd48a8e2da0838f7f75d76
unknown <clesio0420@gmail.com>
2023-08-15 15:31:50 -0300

    Projeto Banco 2

--- conta/src/conta/Menu.java
+++ conta/src/conta/Menu.java
@@ -1,37 +1,41 @@
 package conta;
 
-import java.io.IOException;
+import java.io.IOException; 
 import java.util.InputMismatchException;
 import java.util.Scanner;
 import conta.model.Conta;
 import conta.model.ContaCorrente;
 import conta.model.ContaPoupanca;
 import conta.util.Cores;
+import conta.controller.ContaController;
 
 public class Menu {
 
 	public static void main(String[] args) {
-
 		
-		// Teste da Classe Conta Conrrente
-		ContaCorrente cc1 = new ContaCorrente(2, 123, 1, "Mariana", 150000.0f, 1000.0f);
-		cc1.vizualizar();
-		cc1.sacar(12000.0f);
-		cc1.vizualizar();
-		cc1.depositar(5000.0f);
-		cc1.vizualizar();
-
-		// Teste da Classe Conta Corrente
-		ContaPoupanca cp1 = new ContaPoupanca(3, 123, 0, "Victor", 100000.0f, 15);
-		cp1.vizualizar();
-		cp1.sacar(1000.0f);
-		cp1.vizualizar();
-		cp1.depositar(5000.0f);
-		cp1.vizualizar();
-
+		ContaController contas = new ContaController();
+		
 		Scanner leia = new Scanner(System.in);
 
-		int opcao;
+		int opcao, numero, agencia, tipo, aniversario;
+		String titular;
+		float saldo, limite;
+		
+		System.out.println("\nCriar Contas\n");
+		
+		ContaCorrente cc1 = new ContaCorrente(contas.gerarNumero(), 123, 1,  "João Silva", 1000f, 100.0f);
+		contas.cadastrar(cc1);
+		
+		ContaCorrente cc2 = new ContaCorrente(contas.gerarNumero(), 124, 1,"Maria Silva", 2000f, 100.0f);
+		contas.cadastrar(cc2);
+		
+		ContaPoupanca cp1 = new ContaPoupanca(contas.gerarNumero(), 125, 2,"Mariana dos Santos", 4000f, 12);
+		contas.cadastrar(cp1);
+		
+		ContaPoupanca cp2 = new ContaPoupanca(contas.gerarNumero(), 126, 2,"Juliana Ramos", 8000f, 15);
+		contas.cadastrar(cp2);
+		
+		contas.listarTodas();
 
 		while (true) {
 
@@ -66,7 +70,7 @@ public class Menu {
 			}
 
 			if (opcao == 9) {
-				System.out.println(Cores.TEXT_WHITE_BOLD + "\nBanco Universal - O seu Futuro começa aqui!");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "\nBanco Universal - Um mundo de possibilidades!");
 				leia.close();
 				System.exit(0);
 			}
@@ -74,12 +78,37 @@ public class Menu {
 			switch (opcao) {
 			case 1:
 				System.out.println(Cores.TEXT_WHITE + "Criar Conta\n\n");
-
+				System.out.println("Digite o Numero da Agência: ");
+				agencia = leia.nextInt();
+				System.out.println("Digite o Nome do Titular: ");
+				leia.skip("\\R?");
+				titular = leia.nextLine();
+				
+				do {
+					System.out.println("Digite o tipo de conta(1-CC ou 2-CP): ");
+					tipo = leia.nextInt();
+				}while(tipo < 1 && tipo > 2);
+				
+				System.out.println("Digite o saldo da conta(R$): ");
+				saldo = leia.nextFloat();
+				
+				switch(tipo) {
+				case 1 -> {
+					System.out.println("Digite o limite de Crédito(R$): ");
+					limite = leia.nextFloat();
+					contas.cadastrar(new ContaCorrente(contas.gerarNumero(), agencia, tipo, titular, saldo, limite));
+				}
+				case 2 ->{
+					System.out.println("Digite o dia do aniversario da Conta: ");
+					aniversario = leia.nextInt();
+					contas.cadastrar(new ContaPoupanca(contas.gerarNumero(), agencia, tipo, titular, saldo, aniversario));
+				}}
+				
 				keyPress();
 				break;
 			case 2:
 				System.out.println(Cores.TEXT_WHITE + "Listar todas as Contas\n\n");
-
+				contas.listarTodas();
 				keyPress();
 				break;
 			case 3:
